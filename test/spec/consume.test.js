bc15b201a2c0d2051406f38f3730215383eeb086
Thanasis Polychronakis <thanpolas@gmail.com>
2017-03-01 18:33:09 +0200

    test multiple consumers (40)

--- test/spec/consume.test.js
+++ test/spec/consume.test.js
@@ -3,6 +3,7 @@
  */
 const chai = require('chai');
 const expect = chai.expect;
+var Promise = require('bluebird');
 
 const testLib = require('../lib/tester.lib');
 
@@ -80,4 +81,72 @@ describe('Job Consume', function() {
       this.sqs.createJob(data);
     });
   });
+
+  describe('40 jobs concurrent consumption', function() {
+    beforeEach(function() {
+      const opts = {
+        sqsUrl: testLib.SQS_URL,
+        concurrentOpsLimit: 40,
+        logger: testLib.log,
+      };
+
+      this.sqs = sqsLib(opts);
+    });
+
+    beforeEach(function() {
+      return this.sqs.init();
+    });
+
+    beforeEach(function() {
+      return this.sqs.purge();
+    });
+
+    afterEach(function() {
+      return this.sqs.dispose();
+    });
+
+
+    it('Should process 40 jobs concurrently', function(done) {
+      const data = {
+        a: 1,
+      };
+
+      var counter = 0;
+
+      // Start fetching with a delay so jobs have time
+      // to be created
+      setTimeout(function() {
+        this.sqs.startFetch(function(jobItem) {
+          return new Promise(function(resolve) {
+            counter++;
+            expect(jobItem).to.deep.equal(data);
+            setTimeout(function() {
+              resolve();
+            }, 500);
+          })
+            .then(function() {
+              // we cannot hope that all 4 long-poll requests will get
+              // full 10 jobs each, some will get fewer. At best we
+              // can hope that at the first run at least 20 jobs
+              // were processed (vs 10 or less).
+              expect(counter).to.be.at.least(20);
+            })
+            .catch(done);
+        });
+      }.bind(this), 2000);
+
+      Promise.resolve(new Array(40))
+        .bind(this)
+        .map(function() {
+          this.sqs.createJob(data);
+        });
+
+      // fixed time resolving
+      setTimeout(function() {
+        expect(counter).to.equal(40);
+        done();
+      }.bind(this), 10000);
+    });
+  });
+
 });
