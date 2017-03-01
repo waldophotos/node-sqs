71d7c36e14023b36a15dd4fd93aa9119c99f1177
Thanasis Polychronakis <thanpolas@gmail.com>
2017-03-01 18:08:08 +0200

    test and fix single message consumption

--- test/spec/consume.test.js
+++ test/spec/consume.test.js
@@ -9,34 +9,75 @@ const testLib = require('../lib/tester.lib');
 const sqsLib = require('../..');
 
 describe('Job Consume', function() {
-  beforeEach(function() {
-    const opts = {
-      sqsUrl: testLib.SQS_URL,
-      concurrentOpsLimit: 1,
-      logger: testLib.log,
-    };
 
-    this.sqs = sqsLib(opts);
-  });
+  describe('Single job consumption', function() {
+    beforeEach(function() {
+      const opts = {
+        sqsUrl: testLib.SQS_URL,
+        concurrentOpsLimit: 1,
+        logger: testLib.log,
+      };
 
-  beforeEach(function() {
-    return this.sqs.init();
-  });
-
-  beforeEach(function() {
-    return this.sqs.purge();
-  });
-
-
-  it('Should get a message', function(done) {
-    const data = {
-      a: 1,
-    };
-    this.sqs.startFetch(function(jobItem) {
-      expect(jobItem).to.deep.equal(data);
-      done();
+      this.sqs = sqsLib(opts);
     });
-    this.sqs.createJob(data);
-  });
 
+    beforeEach(function() {
+      return this.sqs.init();
+    });
+
+    beforeEach(function() {
+      return this.sqs.purge();
+    });
+
+    afterEach(function() {
+      return this.sqs.dispose();
+    });
+
+
+    it('Should get a message', function(done) {
+      const data = {
+        a: 1,
+      };
+      this.sqs.startFetch(function(jobItem) {
+        return new Promise(function(resolve) {
+          console.log('message test 1');
+          expect(jobItem).to.deep.equal(data);
+          resolve();
+          done();
+        }).catch(done);
+      });
+      this.sqs.createJob(data);
+    });
+    it('Should get one message at a time', function(done) {
+      const data = {
+        a: 1,
+      };
+
+      var counter = 0;
+      var inProcess = false;
+      this.sqs.startFetch(function(jobItem) {
+        return new Promise(function(resolve) {
+          counter++;
+          expect(inProcess).to.be.false;
+          inProcess = true;
+          expect(jobItem).to.deep.equal(data);
+          setTimeout(function() {
+            inProcess = false;
+            resolve();
+          }, 500);
+        })
+          .then(function() {
+            if (counter === 4) {
+              done();
+            }
+          })
+          .catch(done);
+      });
+
+      this.sqs.createJob(data);
+      this.sqs.createJob(data);
+      this.sqs.createJob(data);
+      this.sqs.createJob(data);
+    });
+  });
 });
